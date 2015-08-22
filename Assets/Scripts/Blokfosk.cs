fa1487c20a44fe795337da01292cf1e1f1d41298
Backman <Arrebarree@gmail.com>
2015-08-22 20:12:02 +0200

    BLOKFOSK HYPA!

--- Assets/Scripts/Blokfosk.cs
+++ Assets/Scripts/Blokfosk.cs
@@ -26,25 +26,45 @@ public class TentacleData
 
 public class Blokfosk : MonoBehaviour
 {
+	#if UNITY_STANDALONE_OSX
 	private const string RightStickHorizontal = "RightStickX";
 	private const string RightStickVertical = "RightStickY";
 
 	private const string LeftStickHorizontal = "LeftStickX";
 	private const string LeftStickVertical = "LeftStickY";
 
+	private const string HypeButton = "Hype";
+	#elif UNITY_STANDALONE_WIN
+	private const string RightStickHorizontal = "RightStickX_WIN";
+	private const string RightStickVertical = "RightStickY_WIN";
+
+	private const string LeftStickHorizontal = "LeftStickX_WIN";
+	private const string LeftStickVertical = "LeftStickX_WIN";
+
+	private const string HypeButton = "Fire1";
+	#endif
+
 	public TentacleData TentacleSettings;
-	public GameObject Body;
 	public Tentacle LeftTentacle;
 	public Tentacle RightTentacle;
 
 	public CircleCollider2D LeftTentacleBounds;
 	public CircleCollider2D RightTentacleBounds;
 
+	public float MaxHype = 5f;
+	public float HypeBuildUp = 1f;
+
 	public bool InAir { get; set; }
+
+	private Rigidbody2D _rb;
+
+	private bool _isHyping;
+	[SerializeField]
+	private float _currentHype;
 
 	private void Awake ()
 	{
-		
+		_rb = GetComponent<Rigidbody2D> ();
 	}
 
 	private void Start ()
@@ -66,16 +86,16 @@ public class Blokfosk : MonoBehaviour
 		ApplyTentacleConstraint (LeftTentacle, LeftTentacleBounds);
 		ApplyTentacleConstraint (RightTentacle, RightTentacleBounds);
 
-		var diff = LeftTentacle.Target.position - RightTentacle.Target.position;
-		
+//		ApplyRotation ();
 
-		var angle = Vector3.Angle (LeftTentacle.Target.position, RightTentacle.Target.position);
+		HypeInput ();
+	}
 
-		var leftDiff = Body.transform.position - LeftTentacle.Target.position;
-		var rightDiff = Body.transform.position - RightTentacle.Target.position;
-
-		var speed = InAir ? TentacleSettings.InAir.RotationSpeed : TentacleSettings.UnderWater.RotationSpeed;
-		Body.transform.Rotate (0f, 0f, angle * speed * Time.deltaTime);
+	private void FixedUpdate ()
+	{
+		if (_isHyping) {
+			_rb.MovePosition (_rb.position + Vector2.down * Time.deltaTime);
+		}
 	}
 
 	private void TentacleInput (Tentacle tentacle, Vector2 input)
@@ -100,6 +120,51 @@ public class Blokfosk : MonoBehaviour
 			
 			tentacle.SetTarget (center + delta.normalized * radius);
 		}
+	}
+
+	private void ApplyRotation ()
+	{
+		var diff = LeftTentacle.Target.position - RightTentacle.Target.position;
+
+
+		var angle = Vector3.Angle (LeftTentacle.Target.position, RightTentacle.Target.position);
+
+		var leftDiff = transform.position - LeftTentacle.Target.position;
+		var rightDiff = transform.position - RightTentacle.Target.position;
+
+		var speed = InAir ? TentacleSettings.InAir.RotationSpeed : TentacleSettings.UnderWater.RotationSpeed;
+		transform.Rotate (0f, 0f, angle * speed * Time.deltaTime);
+	}
+
+	private void HypeInput ()
+	{
+		var hypeDown = Input.GetButton (HypeButton);
+		if (hypeDown) {
+			_isHyping = true;
+		}
+
+		if (!hypeDown && _isHyping) {
+			_isHyping = false;
+			MLGHype ();
+		}
+
+		if (_isHyping) {
+			BuildHype ();
+		}
+	}
+
+	private void BuildHype ()
+	{
+		Debug.Log ("HYPE BUILDUP");
+		_currentHype += HypeBuildUp * Time.deltaTime;
+	}
+
+	private void MLGHype ()
+	{
+		// SHOOT THE FAKKING BLOKFOSK INTO THE AIR!
+		Debug.LogFormat ("HYPEEEE: {0}/{1}", _currentHype, MaxHype);
+		_rb.AddForce (transform.up * _currentHype * 1000f);
+		_currentHype = 0f;
 	}
 
 	private Vector2 GetJoystickAxis (string horizontal, string vertical)
