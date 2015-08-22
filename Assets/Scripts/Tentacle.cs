d693761702654562404554e2ac7874003db8404e
Backman <Arrebarree@gmail.com>
2015-08-22 16:04:44 +0200

    Blokfosk tentacles

---
+++ Assets/Scripts/Tentacle.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tentacle : MonoBehaviour
+{
+	public SimpleCCD CCD;
+}
--- Assets/Scripts/blokfosk.cs
+++ Assets/Scripts/blokfosk.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class blokfosk : MonoBehaviour {
+public class Blokfosk : MonoBehaviour
+{
+	public Tentacle LeftTentacle;
+	public Tentacle RightTentacle;
 
-	// Use this for initialization
-	void Start () {
+	public BoxCollider2D LeftTentacleBounds;
+	public BoxCollider2D RightTentacleBounds;
 	
+	private Transform _leftTentacleTarget;
+	private Transform _rightTentacleTarget;
+
+	public void Awake ()
+	{
+		_leftTentacleTarget = LeftTentacle.GetComponent<SimpleCCD> ().target;
+		_rightTentacleTarget = RightTentacle.GetComponent<SimpleCCD> ().target;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Update ()
+	{
+
 	}
 }
