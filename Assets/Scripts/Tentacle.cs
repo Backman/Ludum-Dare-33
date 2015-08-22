f6a7df200fd63980deec303704094cfe90afa3f3
Backman <Arrebarree@gmail.com>
2015-08-22 17:52:17 +0200

    First blokfosk thingy

--- Assets/Scripts/Tentacle.cs
+++ Assets/Scripts/Tentacle.cs
@@ -4,4 +4,25 @@ using System.Collections;
 public class Tentacle : MonoBehaviour
 {
 	public SimpleCCD CCD;
+	[HideInInspector]
+	public Transform Target;
+
+	private void Awake ()
+	{
+		if (!CCD) {
+			CCD = GetComponent<SimpleCCD> ();
+		}
+
+		Target = CCD.target;
+	}
+
+	public void MoveTarget (Vector2 move)
+	{
+		Target.position += new Vector3 (move.x, move.y);
+	}
+
+	public void SetTarget (Vector3 newPos)
+	{
+		Target.position = newPos;
+	}
 }
