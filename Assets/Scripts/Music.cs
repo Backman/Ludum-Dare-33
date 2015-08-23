a80f6df9fa3edd8546cb79aafcfadcb0d3101bf7
Backman <Arrebarree@gmail.com>
2015-08-23 21:05:01 +0200

    Build

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Music : MonoBehaviour {
+public class Music : MonoBehaviour
+{
 
 	public Blokfosk Player;
 	private GameObject MusicPlayer;
 	private AudioSource[] Songs;
-	public float transition= 0;
-	[Range(0.0f, 1.0f)]
+	public float transition = 0;
+	[Range (0.0f, 1.0f)]
 	public float Musicvolume = 0.33f;
 	public float fadespeed = 0.025f;
-	[Range(0.0f, 1.0f)]
+	[Range (0.0f, 1.0f)]
 	public float sfxv = 1f;
 	public static Music instance;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		instance = this;
 		Player = FindObjectOfType<Blokfosk> ();
 		MusicPlayer = GameObject.Find ("MusicPlayer");
@@ -22,48 +24,50 @@ public class Music : MonoBehaviour {
 
 	}
 
-	public static AudioSource PlayClipAtPoint(AudioClip clip, Vector3 pos, float volume) {
-		var tempGO = new GameObject("TempAudio"); // create the temp object
+	public static AudioSource PlayClipAtPoint (AudioClip clip, Vector3 pos, float volume)
+	{
+		var tempGO = new GameObject ("TempAudio"); // create the temp object
 		tempGO.transform.position = pos; // set its position
-		var aSource = tempGO.AddComponent<AudioSource>(); // add an audio source
+		var aSource = tempGO.AddComponent<AudioSource> (); // add an audio source
 		aSource.clip = clip; // define the clip
 		aSource.volume = volume;
 		aSource.spatialBlend = 1.0f;
-		aSource.minDistance = 100f;
-		aSource.maxDistance = 200f;
+		aSource.minDistance = 5f;
+		aSource.maxDistance = 100f;
 		// set other aSource properties here, if desired
-		aSource.Play(); // start the sound
-		Destroy(tempGO, clip.length); // destroy object after clip duration
+		aSource.Play (); // start the sound
+		Destroy (tempGO, clip.length); // destroy object after clip duration
 		return aSource; // return the AudioSource reference
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		Songs [1].volume = transition * Musicvolume;
-		Songs [0].volume =(1- transition) * Musicvolume;
+		Songs [0].volume = (1 - transition) * Musicvolume;
 		Songs [2].volume = transition * Musicvolume;
 
 
 		float threshold = Player.VelocitySettings.BobThreshold;
 
-		if (Player.transform.position.y < threshold -0.75f) {
-			if(!Songs[2].isPlaying){
-			Songs[2].Play();
+		if (Player.transform.position.y < threshold - 0.75f) {
+			if (!Songs [2].isPlaying) {
+				Songs [2].Play ();
 			}
 			//Songs[0].volume = (1 - Player.Hype.NormalizedHype) * Musicvolume;
 			//Songs[1].volume = Player.Hype.NormalizedHype * Musicvolume;
-			transition += fadespeed *Time.deltaTime;
+			transition += fadespeed * Time.deltaTime;
 	
 		}
-		if (Player.transform.position.y > threshold -0.75f) {
-			if(Songs[2].isPlaying && Songs[2].volume == 0f){
-				Songs[2].Stop();
+		if (Player.transform.position.y > threshold - 0.75f) {
+			if (Songs [2].isPlaying && Songs [2].volume == 0f) {
+				Songs [2].Stop ();
 			}
 		
-			transition -= 10*fadespeed *Time.deltaTime;
+			transition -= 10 * fadespeed * Time.deltaTime;
 			//Songs[0].volume = Musicvolume;
 			//Songs[1].volume = 0;
 		}
-		transition = Mathf.Clamp01(transition);
+		transition = Mathf.Clamp01 (transition);
 	}
 }
