274208732606cd38b2d4fd6f6cbef94d39b62094
Konrad Petersson <da_melta@hotmail.com>
2015-08-24 14:39:37 +0200

    More dynamic pitching of explosions

--- Assets/Scripts/Explodable.cs
+++ Assets/Scripts/Explodable.cs
@@ -23,7 +23,7 @@ public class Explodable : MonoBehaviour
 	{
 		if (ExplodeClip)
 		{
-			Music.PlayClipAtPoint(ExplodeClip, pos, Music.instance.sfxv, Random.Range(0.90f, 1.10f));
+			Music.PlayClipAtPoint(ExplodeClip, pos, Music.instance.sfxv, Random.Range(0.50f, 1.50f));
 		}
 
 		for (int i = 0; i < Explosions.Length; i++)
