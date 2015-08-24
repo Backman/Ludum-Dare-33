fd6fd9e0f7ab59559c05e80d1af85ab71d8d1d1a
Backman <Arrebarree@gmail.com>
2015-08-25 00:15:22 +0200

    helelele

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,6 +6,7 @@ public class HealthBar : MonoBehaviour
 {
 	public Image Bar;
 	public Image Blink;
+	public float BlinkDuration = 0.5f;
 
 	private float _originWidth;
 	private int _maxHealth;
@@ -26,6 +27,19 @@ public class HealthBar : MonoBehaviour
 		var healthPercentage = currentHealth / _maxHealth;
 
 		_originWidth *= healthPercentage;
+		StartCoroutine (DoBlink ());
+	}
 
+	private IEnumerator DoBlink ()
+	{
+		var t = 0f;
+		var color = Blink.color;
+		color.a = 1f;
+		Blink.color = color;
+
+		yield return new WaitForSeconds (BlinkDuration);
+
+		color.a = 0f;
+		Blink.color = color;
 	}
 }
