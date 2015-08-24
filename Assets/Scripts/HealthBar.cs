75b458441e91a7e398e9a418644ad34051276199
Backman <Arrebarree@gmail.com>
2015-08-25 00:11:09 +0200

    Helath bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+	public Image Bar;
+	public Image Blink;
+
+	private float _originWidth;
+	private int _maxHealth;
+
+	private void Awake ()
+	{
+		_originWidth = Bar.rectTransform.rect.width;
+	}
+
+	private void Start ()
+	{
+		_maxHealth = Blokfosk.Instance.MaxHealth;
+		GameLogic.Instance.OnBlokDamage += OnBlokDamage;
+	}
+
+	private void OnBlokDamage (int currentHealth)
+	{
+		var healthPercentage = currentHealth / _maxHealth;
+
+		_originWidth *= healthPercentage;
+
+	}
+}
