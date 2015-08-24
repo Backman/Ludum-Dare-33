b96f799b5a968b8e67d88148e4ba9dcfcecbab5a
Karl Bergström <karlb@stunlockstudios.com>
2015-08-24 13:39:50 +0200

    Fixed blink

--- Assets/Scripts/BlinkManager.cs
+++ Assets/Scripts/BlinkManager.cs
@@ -11,7 +11,7 @@ public class BlinkManager : MonoBehaviour
         public Renderer[] Renderers;
     }
     Dictionary<GameObject, BlinkState> _Blinks = new Dictionary<GameObject, BlinkState>();
-    MaterialPropertyBlock _Block ;
+    MaterialPropertyBlock _Block;
 
 
     List<GameObject> toRemove = new List<GameObject>();
@@ -30,18 +30,19 @@ public class BlinkManager : MonoBehaviour
             if (Time.unscaledTime > state.EndTime)
             {
                 toRemove.Add(blink.Key);
+                continue;
             }
             float duration = state.EndTime - state.StartTime;
 
 
             float t = (state.EndTime - Time.unscaledTime) / duration;
-            _Block.Clear();
             var color = state.Color;
-            color.a *=  t;
-            _Block.SetColor("_BlinkColor", color);
+            color.a *= t;
             for (int i = 0; i < state.Renderers.Length; i++)
             {
                 var renderer = state.Renderers[i];
+                renderer.GetPropertyBlock(_Block);
+                _Block.SetColor("_BlinkColor", color);
                 renderer.SetPropertyBlock(_Block);
             }
         }
@@ -49,17 +50,19 @@ public class BlinkManager : MonoBehaviour
         for (int i = 0; i < toRemove.Count; i++)
         {
             var state = _Blinks[toRemove[i]];
-            
+
             for (int j = 0; j < state.Renderers.Length; j++)
             {
                 var renderer = state.Renderers[j];
+                renderer.GetPropertyBlock(_Block);
+                _Block.SetColor("_BlinkColor", new Color(0,0,0,0));
                 renderer.SetPropertyBlock(_Block);
             }
 
             _Blinks.Remove(toRemove[i]);
         }
         toRemove.Clear();
-        
+
     }
 
     public void AddBlink(GameObject source, Color color, float duration)
