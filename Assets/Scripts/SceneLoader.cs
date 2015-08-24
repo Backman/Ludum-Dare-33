8c149069975138b3bf0dc105b67f888c6430c55b
Backman <Arrebarree@gmail.com>
2015-08-24 23:15:16 +0200

    Intro/Splash screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+	public string SceneToLoad;
+
+	public FadeSettings FadeSettings;
+
+	private bool _loadScene;
+
+	private void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Fader.Instance.Fade (FadeSettings);
+			_loadScene = true;
+		}
+
+		if (_loadScene && FadeSettings.DoneFading) {
+			Application.LoadLevel (SceneToLoad);
+		}
+	}
+}
