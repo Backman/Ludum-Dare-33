035008ed64dcf49ecd30293d275b90772f25e93e
Arvid Backman <arrebarree@gmail.com>
2015-08-25 15:05:55 +0200

    Fixes

--- Assets/Scripts/WrekFaceHandler.cs
+++ Assets/Scripts/WrekFaceHandler.cs
@@ -4,117 +4,119 @@ using System.Collections.Generic;
 
 public class WrekFaceHandler : MonoBehaviour
 {
-    public float RekDuration = 0.08f;
-    [Range(0.0f, 1.0f)]
-    public float FreezeValue = 0.0f;
-    public Color BlinkColor;
-    public float BlinkDuration;
-    public float BlokfoskBlinkDuration;
-    public AnimationCurve BlinkCurve;
-    public float MaxDistance = 10f;
-    public float MinDistance = 5f;
-    public float ZoomMinDistance = 12f;
-    public float ZoomLerpModifier = 20f;
+	public float RekDuration = 0.08f;
+	[Range(0.0f, 1.0f)]
+	public float FreezeValue = 0.0f;
+	public Color BlinkColor;
+	public float BlinkDuration;
+	public float BlokfoskBlinkDuration;
+	public AnimationCurve BlinkCurve;
+	public float MaxDistance = 10f;
+	public float MinDistance = 5f;
+	public float ZoomMinDistance = 12f;
+	public float ZoomLerpModifier = 20f;
 
-    [System.Serializable]
-    public struct ComboTextData
-    {
-        public int MinCombo;
-        public int MaxCombo;
-        public GameObject Prefab;
-    }
-    public ComboTextData[] ComboData;
-    public float ComboTextYOffset;
-    public float ComboTextCircleSize;
-    bool _HasFreezeTime = false;
-    float _LastComboText;
+	[System.Serializable]
+	public struct ComboTextData
+	{
+		public int MinCombo;
+		public int MaxCombo;
+		public GameObject Prefab;
+	}
+	public ComboTextData[] ComboData;
+	public float ComboTextYOffset;
+	public float ComboTextCircleSize;
+	bool _HasFreezeTime = false;
+	float _LastComboText;
 
-    public RekCombo RekCombo;
+	public RekCombo RekCombo;
 
-    void Awake()
-    {
-        GameLogic.Instance.OnRekFace += OnRekFace;
-        GameLogic.Instance.OnBlokDamage += OnBlokDamage;
-        GameLogic.Instance.OnRekComboIncreased += OnRekComboIncreased;
+	void Awake()
+	{
+		GameLogic.Instance.OnRekFace += OnRekFace;
+		GameLogic.Instance.OnBlokDamage += OnBlokDamage;
+		GameLogic.Instance.OnRekComboIncreased += OnRekComboIncreased;
 
-        RekCombo.Init();
+		RekCombo.Init();
 
-    }
+	}
 
-    void OnRekComboIncreased(GameObject obj, int combo)
-    {
-        if (_LastComboText + 0.5f >= Time.unscaledTime)
-            return;
-        if (Random.Range(0f, 1f) > 0.7f)
-            return;
-        List<ComboTextData> valid = new List<ComboTextData>();
-        for (int i = 0; i < ComboData.Length; i++)
-        {
-            if (combo >= ComboData[i].MinCombo && combo <= ComboData[i].MaxCombo)
-            {
-                valid.Add(ComboData[i]);
-            }
-        }
-        if (valid.Count > 0)
-        {
-            ComboTextData comboData = valid[Random.Range(0, valid.Count)];
-            Vector2 unitCircle = Random.insideUnitCircle * ComboTextCircleSize;
-            Vector2 pos = (Vector2)obj.transform.position + unitCircle + new Vector2(0, ComboTextYOffset);
+	void OnRekComboIncreased(GameObject obj, int combo)
+	{
+		if (_LastComboText + 0.5f >= Time.unscaledTime)
+			return;
+		if (Random.Range(0f, 1f) > 0.7f)
+			return;
+		List<ComboTextData> valid = new List<ComboTextData>();
+		for (int i = 0; i < ComboData.Length; i++)
+		{
+			if (combo >= ComboData[i].MinCombo && combo <= ComboData[i].MaxCombo)
+			{
+				valid.Add(ComboData[i]);
+			}
+		}
+		if (valid.Count > 0)
+		{
+			ComboTextData comboData = valid[Random.Range(0, valid.Count)];
+			Vector2 unitCircle = Random.insideUnitCircle * ComboTextCircleSize;
+			Vector2 pos = (Vector2)obj.transform.position + unitCircle + new Vector2(0, ComboTextYOffset);
 
-            TrashMan.spawn(comboData.Prefab, new Vector3(pos.x, pos.y, 30));
-            _LastComboText = Time.unscaledTime;
-        }
-    }
-    void OnBlokDamage(int dmg)
-    {
-        BlinkManager.Instance.AddBlink(Blokfosk.Instance.gameObject, BlinkColor, BlokfoskBlinkDuration);
-    }
+			TrashMan.spawn(comboData.Prefab, new Vector3(pos.x, pos.y, 30));
+			_LastComboText = Time.unscaledTime;
+		}
+	}
+	void OnBlokDamage(int dmg)
+	{
+		BlinkManager.Instance.AddBlink(Blokfosk.Instance.gameObject, BlinkColor, BlokfoskBlinkDuration);
+	}
 
-    void OnRekFace(GameObject obj)
-    {
-        if (obj.activeInHierarchy == false)
-            return;
-        var distance = Vector2.Distance(obj.transform.position, Blokfosk.Instance.transform.position);
+	void OnRekFace(GameObject obj)
+	{
+		if (obj.activeInHierarchy == false)
+			return;
+		var distance = Vector2.Distance(obj.transform.position, Blokfosk.Instance.transform.position);
 
-        var rekDuration = RekDuration;
-        var freezeValue = FreezeValue;
+		var rekDuration = RekDuration;
+		var freezeValue = FreezeValue;
 
-        float distanceMod = Mathf.Clamp01((distance - MinDistance) / MaxDistance);
-        freezeValue *= 1 - distanceMod * 0.7f;
-        rekDuration *= 1 + distanceMod * 0.4f;
+		float distanceMod = Mathf.Clamp01((distance - MinDistance) / MaxDistance);
+		freezeValue *= 1 - distanceMod * 0.7f;
+		rekDuration *= 1 - distanceMod * 0.4f;
 
-        RekCombo.IncreaseRekComboCount(obj);
+		RekCombo.IncreaseRekComboCount(obj);
 
-        BlinkManager.Instance.AddBlink(obj, BlinkColor, BlinkDuration, BlinkCurve);
-        if (_HasFreezeTime == false)
-        {
-            StartCoroutine(FreezeTime(obj, rekDuration, freezeValue, distance));
-            _HasFreezeTime = true;
-        }
-    }
+		BlinkManager.Instance.AddBlink(obj, BlinkColor, BlinkDuration, BlinkCurve);
+		if (_HasFreezeTime == false)
+		{
+			StartCoroutine(FreezeTime(obj, rekDuration, freezeValue, distance));
+			_HasFreezeTime = true;
+		}
+	}
 
-    IEnumerator FreezeTime(GameObject obj, float rekDuration, float freezeValue, float distance)
-    {
-        float startTime = Time.unscaledTime;
-        Time.timeScale = freezeValue;
-        while (startTime + rekDuration > Time.unscaledTime)
-        {
-            if (obj.activeInHierarchy == false)
-                break;
-            if (distance > ZoomMinDistance)
-            {
-                var followCam = Camera.main.GetComponent<FollowCamera>();
-                Vector3 pos = obj.transform.position;
-                followCam.SetTarget(pos, Vector2.zero, 0, 1, ZoomLerpModifier);
-            }
-            yield return null;
-        }
-        Time.timeScale = 1f;
-        _HasFreezeTime = false;
-    }
+	IEnumerator FreezeTime(GameObject obj, float rekDuration, float freezeValue, float distance)
+	{
+		float startTime = Time.unscaledTime;
+		Time.timeScale = freezeValue;
+		bool zooming = false;
+		while (startTime + rekDuration > Time.unscaledTime)
+		{
+			if (!zooming && obj.activeInHierarchy == false)
+				break;
+			if (distance > ZoomMinDistance)
+			{
+				zooming = true;
+				var followCam = Camera.main.GetComponent<FollowCamera>();
+				Vector3 pos = obj.transform.position;
+				followCam.SetTarget(pos, Vector2.zero, 0, 1, ZoomLerpModifier);
+			}
+			yield return null;
+		}
+		Time.timeScale = 1f;
+		_HasFreezeTime = false;
+	}
 
-    public void Update()
-    {
-        RekCombo.Update();
-    }
+	public void Update()
+	{
+		RekCombo.Update();
+	}
 }
