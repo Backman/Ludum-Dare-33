582e5b20a9b57d94f1a30aeb108a66f6c8e8855d
Arvid Backman <arrebarree@gmail.com>
2015-08-27 10:29:19 +0200

    Score shake!

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,16 +5,18 @@ using System.Collections.Generic;
 
 public class ScoreManager : MonoBehaviour
 {
-	public Text ScoreCountText;
+	private const string ScoreString = "Score: ";
 	public Text ScoreText;
+	public Text ScoreTextDropShadow;
 	public float TimeUntilAddingScore = 0.8f;
 	public float ScoreTickSpeed = 0.05f;
 
-	public float BlinkSpeedIncrease = 1f;
 	public AnimationCurve BlinkSpeedCurve;
-	public bool BlinkWholeText = false;
-	public float ColorGradientSpeed = 1f;
 	public Gradient ColorGradient;
+	public float TextIntensityBuildUpSpeed = 1f;
+
+	public AnimationCurve XShakeCurve;
+	public AnimationCurve YShakeCurve;
 
 	private int _currentScore;
 	private bool _addingScore;
@@ -23,6 +25,7 @@ public class ScoreManager : MonoBehaviour
 	private int _scoreToAdd;
 	private Color _blinkColor;
 	private float _blinkSpeed;
+
 
 	private float _blinkTime;
 
@@ -52,34 +55,57 @@ public class ScoreManager : MonoBehaviour
 		var t = 0f;
 		_addingScore = true;
 
+		var scoreTextPos = ScoreText.transform.position;
+		var dropShadowPos = ScoreTextDropShadow.transform.position;
+
+		var scoreTextOriginPos = scoreTextPos;
+		var dropShadowOriginPos = dropShadowPos;
+
 		while (scoreToAdd > 0)
 		{
-			t += Time.deltaTime * ColorGradientSpeed;
+			t += Time.deltaTime * TextIntensityBuildUpSpeed;
 			_blinkColor = ColorGradient.Evaluate(t);
 			_blinkSpeed = BlinkSpeedCurve.Evaluate(t);
+
+			scoreTextPos.x += XShakeCurve.Evaluate(t);
+			scoreTextPos.y += YShakeCurve.Evaluate(t);
+
+			dropShadowPos.x += XShakeCurve.Evaluate(t);
+			dropShadowPos.y += YShakeCurve.Evaluate(t);
+
+			ScoreText.transform.position = scoreTextPos;
+			ScoreTextDropShadow.transform.position = dropShadowPos;
+
 			_currentScore += 1;
 			scoreToAdd -= 1;
-			ScoreCountText.text = _currentScore.ToString();
+			var text = _currentScore.ToString();
+			ScoreText.text = text;
+			ScoreTextDropShadow.text = text;
 			yield return new WaitForSeconds(ScoreTickSpeed);
+
+			ScoreText.transform.position = scoreTextOriginPos;
+			ScoreTextDropShadow.transform.position = dropShadowOriginPos;
 		}
 		_addingScore = false;
 	}
 
 	private IEnumerator TextBlink(int scoreToAdd)
 	{
-		var scoreCountColor = ScoreCountText.color;
-		var scoreTextColor = ScoreText.color; 
+		var scoreTextColor = ScoreText.color;
+		var dropShadowColor = ScoreTextDropShadow.color;
+		var dropShadowAlpha = dropShadowColor.a;
 		var blink = true;
 
 		while (_addingScore)
 		{
-			ScoreCountText.color = blink ? _blinkColor : scoreCountColor;
+			var col = blink ? _blinkColor : dropShadowColor;
+			col.a = dropShadowAlpha;
+			ScoreTextDropShadow.color = col;
 			ScoreText.color = blink ? _blinkColor : scoreTextColor;
 			blink = !blink;
 			yield return new WaitForSeconds(_blinkSpeed);
 		}
 
-		ScoreCountText.color = scoreCountColor;
 		ScoreText.color = scoreTextColor;
 	}
 }
